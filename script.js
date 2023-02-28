91f7a65ab9a7786efc977611bb32e11c58ed992b
jokatty <jyotikattani@gmail.com>
2023-02-28 18:53:29 +0100

    add get date function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("script.js is connected");
+/**
+ * function gives today's date
+ */
+
+function getTodaysDate(){
+  let date = new Date();
+  let currentDate = date.getDate();
+  let currentMonth = date.getMonth();
+  let currentYear = date.getFullYear();
+  return {currentDate, currentMonth, currentYear};
+}
+
+// display today's date
+const h3 = document.createElement("h3");
+document.body.appendChild(h3);
+let displayDate = getTodaysDate();
+h3.innerText = `Today's date is ${displayDate.currentDate}/${displayDate.currentMonth}/${displayDate.currentYear}`;
+
+
+
