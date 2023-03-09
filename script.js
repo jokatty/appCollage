d2cabe1140119dc63048e9a84996892d10c72a02
jokatty <jyotikattani@gmail.com>
2023-03-09 19:47:15 +0100

    add current time element

--- script.js
+++ script.js
@@ -16,5 +16,25 @@ document.body.appendChild(h3);
 let displayDate = getTodaysDate();
 h3.innerText = `Today's date is ${displayDate.currentDate}/${displayDate.currentMonth}/${displayDate.currentYear}`;
 
+/**
+ * Finds out current time
+ * @return {object} returns hour,minute and second object
+ */
+
+function getTime(){
+  let now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  return {hours, minutes, seconds}
+}
+
+// Create a time element and display the current time on the page
+const timeEle = document.createElement("h3");
+const time = getTime();
+timeEle.innerText = `${time.hours}:${time.minutes}:${time.seconds}`
+document.body.appendChild(timeEle);
+
+
 
 
